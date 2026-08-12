a97e6e2a9d82f9de681725d730365016da0c08ad
Tactor <119103845+tactordev@users.noreply.github.com>
2026-08-12 17:55:20 +0100

    [LANDER] Landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { SquareArrowOutUpRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="my-6 ml-4 py-4 px-8">
+    <div>
       <p className="font-semibold text-2xl">No content selected.</p>
-      <div onClick={() => window.location.href = "/vbuilder"} className="flex flex-row items-center justify-center gap-2 px-2 py-1 rounded-md mt-2 hover:bg-[var(--primary)]/80 bg-[var(--primary)]/40 hover:cursor-pointer hover:scale-110 active:scale-95 transition-all duration-200">
+      <div onClick={() => window.location.href = "/vbuilder"} className="flex flex-row items-center justify-center gap-2 px-2 py-1 rounded-md mt-2 hover:bg-[var(--primary)]/80 bg-[var(--primary)]/40 hover:cursor-pointer hover:scale-110 active:scale-95 transition-all duration-200 w-fit px-8">
         <p className="text-xl">Regulation builder</p>
         <SquareArrowOutUpRight className="w-4 h-4" />
       </div>
